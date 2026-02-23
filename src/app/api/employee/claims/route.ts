3348282251e55da324d894e4840ad195f83a2f3c
Dorantino1 <seth.partnerdeals@gmail.com>
2026-02-22 23:49:04 -0400

    fix: add eslint to vercel

--- src/app/api/employee/claims/route.ts
+++ src/app/api/employee/claims/route.ts
@@ -1,7 +1,19 @@
-import { getEmployeeClaims } from "@/tools/DataManager"
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import { getEmployeeClaims } from "@/tools/DataManager";
 
 export async function GET() {
-    const claims = await getEmployeeClaims()
-    return NextResponse.json({ success: true, claims })
+    try {
+        const claims = await getEmployeeClaims();
+
+        return NextResponse.json({
+            success: true,
+            claims
+        });
+
+    } catch (error: any) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+        );
+    }
 }
