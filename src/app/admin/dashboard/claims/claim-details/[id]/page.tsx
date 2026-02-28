bf327a747b23d7c4109920573c4f6facc14f7e2c
Dorantino1 <seth.partnerdeals@gmail.com>
2026-02-28 10:59:31 -0400

    fix: update claim details page to use getAdminClaims

--- src/app/admin/dashboard/claims/claim-details/[id]/page.tsx
+++ src/app/admin/dashboard/claims/claim-details/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { getClaims } from "@/tools/DataManager";
+import { getAdminClaims } from "@/tools/DataManager";
 import ClaimDetails from "./claimDetails";
 
 
 
 export default async function page() {
-    const claims: any = await getClaims();
+    const claims: any = await getAdminClaims();
 
 
 
